423925a85baba8f5d0152ec67915c167a96c8aee
Roger Z <the.roger.zou@gmail.com>
2013-11-06 20:08:26 -0500

    finished spell checker

--- exercises-spellchecker/dictionary.py
+++ exercises-spellchecker/dictionary.py
@@ -16,22 +16,25 @@ def load(dictionary_name):
     Each line in the file contains exactly one word.
     """
     # TODO: remove the pass line and write your own code
-    pass
-
+    f = open(dictionary_name, "r")
+    words = set()
+    for line in f:
+        stripper = line.strip() #takes away new lines
+        words.add(stripper)
+    f.close()
+    return words
 def check(dictionary, word):
     """
     Returns True if `word` is in the English `dictionary`.
     """
-    pass
-
+    return word in dictionary
 def size(dictionary):
     """
     Returns the number of words in the English `dictionary`.
     """
-    pass
-
+    return len(dictionary)
 def unload(dictionary):
     """
     Removes everything from the English `dictionary`.
     """
-    pass
+    dictionary.clear()
